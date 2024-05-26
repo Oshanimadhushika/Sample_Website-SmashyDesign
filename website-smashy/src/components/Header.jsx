b23835a41ca39fcf266a51bd9eb1c7089aefd600
oshani madhushika <oshanimadhushika12@gmail.com>
2024-05-26 11:27:28 +0530

    Added side bar styles

---
+++ website-smashy/src/components/Header.jsx
@@ -0,0 +1,200 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logoN.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen((prevState) => !prevState);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className="p-5">
+      <div className="flex items-center justify-between">
+        <div className="flex justify-start ">
+          <img src={logo} alt="Neon Strategies Logo" className="w-12 h-12" />
+          <h1 className="text-black text-sm w-2 font-semibold">
+            Neon Strategies
+          </h1>
+        </div>
+
+        <div className=" flex-grow flex xl:justify-center lg:justify-center items-center sm:justify-end md:justify-center justify-end">
+          <ul className="text-black text-lg  gap-8 hidden md:flex lg:flex xl:flex items-center">
+            <li>
+              <Link
+                className="text-decoration-none transition-colors hover:text-blue-500"
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+
+            <li className="relative">
+              <button
+                onClick={toggleDropdown}
+                className="text-decoration-none transition-colors hover:text-blue-500  flex items-center"
+              >
+                Services <span className="ml-2">&#9662;</span>
+              </button>
+              {isDropdownOpen && (
+                <ul className="absolute mt-2 py-2 w-48 bg-white border rounded shadow-lg">
+                  <li>
+                    <Link
+                      className="block px-4 py-2 text-black hover:bg-gray-200"
+                      to="/"
+                    >
+                      Service 1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="block px-4 py-2 text-black hover:bg-gray-200"
+                      to="/"
+                    >
+                      Service 2
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="block px-4 py-2 text-black hover:bg-gray-200"
+                      to="/"
+                    >
+                      Service 3
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+
+            <li>
+              <Link
+                className="text-decoration-none transition-colors hover:text-blue-500 "
+                to="/"
+              >
+                Process
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                className="text-decoration-none transition-colors hover:text-blue-500 "
+                to="/"
+              >
+                FAQ
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                className="text-decoration-none transition-colors hover:text-blue-500 "
+                to="/"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+
+          <div className="md:hidden ">
+            <div className="text-black  lg text-3xl" onClick={toggleSidebar}>
+              {sidebarOpen ? <AiOutlineClose /> : <RxHamburgerMenu />}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className={`sidebar fixed top-20 left-0 h-full w-full bg-white bg-opacity-80 ${
+          sidebarOpen ? "block" : "hidden"
+        }`}
+      >
+        <ul className="text-black text-sm text-center pt-20 gap-7">
+          <li className="py-2 ">
+            <Link
+              className=" hover:bg-blue-700 hover:text-white"
+              to="/"
+              onClick={toggleSidebar}
+              exact
+            >
+              About
+            </Link>
+          </li>
+
+          <li className=" py-2 ">
+            <button
+              onClick={toggleDropdown}
+              className="text-decoration-none transition-colors hover:bg-blue-500  "
+            >
+              Services <span className="ml-2 hover:bg-blue-500">&#9662;</span>
+            </button>
+            {isDropdownOpen && (
+              <ul className="absolute mt-2 py-2 w-48 bg-white border rounded shadow-lg ">
+                <li>
+                  <Link
+                    className="block px-4 py-2 text-black hover:bg-blue-700 hover:text-white"
+                    to="/"
+                  >
+                    Service 1
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="block px-4 py-2 text-black hover:bg-blue-700 hover:text-white"
+                    to="/"
+                  >
+                    Service 2
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="block px-4 py-2 text-black hover:bg-blue-700 hover:text-white"
+                    to="/"
+                  >
+                    Service 3
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li className="py-2">
+            <Link
+              className="text-decoration-none transition-colors hover:bg-blue-700 hover:text-white"
+              to="/"
+              onClick={toggleSidebar}
+              exact
+            >
+              Process
+            </Link>
+          </li>
+          <li className="py-2">
+            <Link
+              className="text-decoration-none transition-colors hover:bg-blue-700 hover:text-white"
+              to="/"
+              onClick={toggleSidebar}
+              exact
+            >
+              FAQ
+            </Link>
+          </li>
+          <li className="py-2">
+            <Link
+              className="text-decoration-none transition-colors  hover:bg-blue-700 hover:text-white"
+              to="/"
+              onClick={toggleSidebar}
+              exact
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
