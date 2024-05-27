eb032c548ac9d259b8c6e2702722fb7b81759e6e
oshani madhushika <oshanimadhushika12@gmail.com>
2024-05-27 20:20:24 +0530

    added sliders

---
+++ website-smashy/src/components/ImageSlider1.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import Img1 from "../assets/img1.png";
+import Img2 from "../assets/img2.png";
+import Img3 from "../assets/img3.png";
+import Img4 from "../assets/img4.png";
+import Img5 from "../assets/img5.png";
+import Img6 from "../assets/img6.png";
+import { useRef } from "react";
+
+const ImageSlider1 = () => {
+  const images = [Img1, Img2, Img3, Img4, Img5, Img6];
+
+  const carouselRef = useRef();
+
+  const handleNext = () => {
+    carouselRef.current.slideNext();
+  };
+
+  return (
+    <div className="">
+      <div className="flex justify-end text-2xl mr-8 ">
+        <FaRegArrowAltCircleRight onClick={handleNext} />
+      </div>
+
+      {/* screens */}
+      <div className="p-5 mt-2 gap-2 justify-center">
+        <AliceCarousel
+          ref={carouselRef}
+          items={images.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              className="w-full h-auto object-cover mr-4 p-2"
+              alt={`slide-${index}`}
+            />
+          ))}
+          responsive={{
+            0: { items: 1 },
+            768: { items: 2 },
+            1024: { items: 2 },
+          }}
+          dotsDisabled={true}
+          buttonsDisabled={true}
+          autoPlay={true}
+          autoPlayInterval={3000}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider1;
--- website-smashy/src/pages/Home.jsx
+++ website-smashy/src/pages/Home.jsx
@@ -3,47 +3,23 @@ import sampleVideo from "../assets/sampleVideo.mp4";
 import React, { useState, useEffect } from "react";
 import VideoComponent from "../components/VideoComponent";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import AboutImge from "../assets/imgAbout.png";
+import AliceCarousel from "react-alice-carousel";
+
+import Img1 from "../assets/img1.png";
+import Img2 from "../assets/img2.png";
+import Img3 from "../assets/img3.png";
+import Img4 from "../assets/img4.png";
+import Img5 from "../assets/img5.png";
+import Img6 from "../assets/img6.png";
+import ImageSlider1 from "../components/ImageSlider1";
+import ImageSlider2 from "../components/ImageSlider2";
 
 const Home = () => {
-  // const wedo=[
-  //     {
-  //         topic: 'Digital Marketing',
-  //         desc: 'At Neon Strategies, we harness the power of digital marketing to elevate your brand and connect you with your target audience. Our comprehensive digital marketing services are designed to drive traffic, increase engagement, and boost conversions.',
-  //         btn1:'SEO',
-  //         btn2:'Social Media Marketing',
-
-  //         btn3:'Content Marketing',
-
-  //     },
-
-  //     {
-  //         topic: 'Digital Marketing',
-  //         desc: '',
-  //         btn1:'SEO',
-  //         btn2:'Social Media Marketing',
-
-  //         btn3:'Content Marketing',
-
-  //     },
-
-  //     {
-  //         topic: 'Digital Marketing',
-  //         desc: '',
-  //         btn1:'SEO',
-  //         btn2:'Social Media Marketing',
-
-  //         btn3:'Content Marketing',
-
-  //     },
-
-  // ]
-
-  const slider = [];
-
   return (
     <>
-      <div class="container mx-auto">
-        <div class="grid grid-cols-12  text-center ">
+      <div class=" w-full">
+        <div class="grid grid-cols-12  text-center container mx-auto">
           <div class=" text-start p-10 col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-9 xl:col-span-7">
             <h1 class="text-black  font-bold font-sans  text-5xl flex flex-col">
               Ignite Your Brand’s Potential <span>with Neon Strategies</span>
@@ -52,7 +28,7 @@ const Home = () => {
         </div>
 
         {/* welcome */}
-        <div class="grid grid-cols-12 gap-5 p-10 pr-3">
+        <div class="grid grid-cols-12 gap-5 p-10 pr-3 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-4  lg:col-span-4 xl:col-span-4   ">
             <h1 className="text-md md:text-sm font-bold font-sans">
               WELCOME TO NEON STRATEGIES
@@ -82,7 +58,7 @@ const Home = () => {
         </div>
 
         {/* video */}
-        <div class="grid grid-cols-12  text-center ">
+        <div class="grid grid-cols-12  text-center container mx-auto">
           <div className="text-start p-4 col-span-12">
             <VideoComponent />
           </div>
@@ -90,7 +66,7 @@ const Home = () => {
 
         {/* what we do */}
 
-        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-6">
+        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-6 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-4  lg:col-span-4 xl:col-span-4   ">
             <h1 className="text-md md:text-sm font-bold font-sans text-start">
               WHAT WE DO
@@ -106,19 +82,19 @@ const Home = () => {
 
         {/* what we do marketing */}
 
-        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3">
+        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-5 xl:col-span-5   ">
             <h1 className="text-xl  font-bold font-sans text-start">
               Digital Marketing
             </h1>
 
-            <p className="font-sans font-normal text-sm mt-3 ">
+            <p className="font-sans font-normal text-sm mt-5 ">
               At Neon Strategies, we harness the power of digital marketing to
               elevate your brand and connect you with your target audience. Our
               comprehensive digital marketing services are designed to drive
               traffic, increase engagement, and boost conversions.
             </p>
-            <div className="flex gap-2 mt-3">
+            <div className="flex gap-2 mt-8">
               <div className="bg-gray-200 rounded-3xl p-2 w-24 h-10 text-gray-700 text-center text-sm">
                 SEO
               </div>
@@ -131,7 +107,7 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="flex mt-6">
+            <div className="flex mt-9">
               <a
                 href=""
                 className="text-decoration-none underline text-sm font-sans font-normal hover:text-red-500"
@@ -145,32 +121,27 @@ const Home = () => {
             </div>
           </div>
 
-          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-6 xl:col-span-6 ">
-            <div className="flex justify-end text-lg">
-              <FaRegArrowAltCircleRight />
-            </div>
-
-            {/* screens */}
-            <div></div>
+          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-7 xl:col-span-7 items-center">
+            <ImageSlider1 />
           </div>
         </div>
 
         {/* what we do Branding */}
 
-        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3">
+        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-5 xl:col-span-5   ">
             <h1 className="text-xl  font-bold font-sans text-start">
               Branding
             </h1>
 
-            <p className="font-sans font-normal text-sm mt-3 ">
+            <p className="font-sans font-normal text-sm mt-5 ">
               At Neon Strategies, we understand that a strong brand is the
               foundation of any successful business. Our branding services are
               designed to create a memorable and impactful brand identity that
               resonates with your target audience and sets you apart from the
               competition.
             </p>
-            <div className="flex gap-2 mt-3">
+            <div className="flex gap-2 mt-8">
               <div className="bg-gray-200 rounded-3xl p-2 w-40 h-10 text-gray-700 text-center text-sm">
                 Brand Strategy
               </div>
@@ -183,7 +154,7 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="flex mt-6">
+            <div className="flex mt-9">
               <a
                 href=""
                 className="text-decoration-none underline text-sm font-sans font-normal hover:text-red-500"
@@ -197,32 +168,27 @@ const Home = () => {
             </div>
           </div>
 
-          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-6 xl:col-span-6 ">
-            <div className="flex justify-end text-lg">
-              <FaRegArrowAltCircleRight />
-            </div>
-
-            {/* screens */}
-            <div></div>
+          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-7 xl:col-span-7 ">
+            <ImageSlider2 />
           </div>
         </div>
 
         {/* what we do Creative Services */}
 
-        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3">
+        <div class="grid grid-cols-12 gap-5 p-10 pr-3 mt-3 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-5 xl:col-span-5   ">
             <h1 className="text-xl  font-bold font-sans text-start">
               Creative Services
             </h1>
 
-            <p className="font-sans font-normal text-sm mt-3 ">
+            <p className="font-sans font-normal text-sm mt-5 ">
               At Neon Strategies, our Creative Services team is dedicated to
               bringing your vision to life with innovative and captivating
               designs. We believe that creativity is at the heart of effective
               marketing, and we strive to create visual content that not only
               captures attention but also drives engagement and results.
             </p>
-            <div className="flex gap-2 mt-3">
+            <div className="flex gap-2 mt-8">
               <div className="bg-gray-200 rounded-3xl p-2 w-40 h-10 text-gray-700 text-center text-sm">
                 Graphic Design
               </div>
@@ -231,7 +197,7 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="flex mt-6">
+            <div className="flex mt-9">
               <a
                 href=""
                 className="text-decoration-none underline text-sm font-sans font-normal hover:text-red-500"
@@ -245,19 +211,14 @@ const Home = () => {
             </div>
           </div>
 
-          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-6 xl:col-span-6 ">
-            <div className="flex justify-end text-lg ">
-              <FaRegArrowAltCircleRight />
-            </div>
-
-            {/* screens */}
-            <div></div>
+          <div class=" col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-7 xl:col-span-7 ">
+            <ImageSlider2 />
           </div>
         </div>
 
         {/* About us */}
 
-        <div class="grid grid-cols-12 gap-8 p-10 pr-3 mt-6">
+        <div class="grid grid-cols-12 gap-8 p-10 pr-3 mt-6 container mx-auto">
           <div class="col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-4 xl:col-span-4   ">
             <h1 className="text-md  font-bold font-sans text-start">
               ABOUT US
@@ -270,23 +231,37 @@ const Home = () => {
             <p className="font-sans font-normal text-md mt-3">
               To light up your brand with cutting-edge marketing strategies that
               drive growth and enhance brand visibility.
-            
             </p>
 
             <p className="mt-3 font-sans font-normal text-md">
-                Neon Strategies is a full-service marketing agency dedicated to
-                energizing your brand through creative and effective marketing
-                solutions. Founded in 2017, we have been at the forefront of
-                marketing innovation, helping businesses of all sizes achieve
-                their goals.
-              </p>
+              Neon Strategies is a full-service marketing agency dedicated to
+              energizing your brand through creative and effective marketing
+              solutions. Founded in 2017, we have been at the forefront of
+              marketing innovation, helping businesses of all sizes achieve
+              their goals.
+            </p>
 
-            <button className="flex bg-blue-900 text-white rounded-3xl p-3 mt-4">
+            <button className="flex bg-blue-800 text-white rounded-3xl p-3 mt-6 text-sm hover:bg-gray-600 ">
               Contact Us Today
               <span className="ml-3 mt-1 text-white">
                 <FaArrowRight />
               </span>
             </button>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-12  mt-3 ">
+          <div class="col-span-12 sm:col-span-12 md:col-span-12  lg:col-span-12 xl:col-span-12   ">
+            <div className="relative   mx-0">
+              <img src={AboutImge} alt="" className="w-full h-auto" />
+
+              <button className="absolute bottom-8 right-20 flex bg-opacity-70  border-2 border-gray-200 text-gray-200 rounded-3xl p-3 mt-6 text-xs hover:bg-blue-300 hover:text-white ">
+                Let's get in touch
+                <span className="ml-3 mt-1 text-gray-200">
+                  <FaArrowRight />
+                </span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
